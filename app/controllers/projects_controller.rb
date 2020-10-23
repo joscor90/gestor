812a257767c53bbed0f910665d653cc6032b6152
Jose Cordero Fuentealba <jocorderf@gmail.com>
2020-10-23 09:50:21 -0300

    Added form to index view

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,10 +1,19 @@
 class ProjectsController < ApplicationController
   def index
+    @project = Project.new 
+    @states = Project.states.keys.to_a
   end
 
   def dashboard
+    @projects = Project.all
   end
 
   def create 
+    @project = Project.create(project_params)
+  end
+
+  private
+  def project_params
+    params.require(:project).permit(:name, :description, :start_on, :end_on, :states)
   end
 end
